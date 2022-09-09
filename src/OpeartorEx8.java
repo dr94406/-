5420425a162bbf93e6b9c317f7269e8de93e68b5
dr94406 <dr94406@naver.com>
2022-09-09 20:30:24 +0900

    [feat] Operator Practice

---
+++ src/OpeartorEx8.java
@@ -0,0 +1,11 @@
+package src;
+
+public class OpeartorEx8 {
+    public static void main(String[] args) {
+        int a = 1_000_000;
+        int b = 2_000_000;
+
+        long c = (long) a * b; // 형변환
+        System.out.println(c);
+    }
+}
---
+++ src/OperatorEx9.java
@@ -0,0 +1,10 @@
+package src;
+
+public class OperatorEx9 {
+    public static void main(String[] args) {
+        long a = 1_000_000 * 1_000_000;
+        long b = 1_000_000 * 1_000_000L;
+        System.out.println("a = " + a); // overflow 발생 !
+        System.out.println("b = " + b);
+    }
+}
