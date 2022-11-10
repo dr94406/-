021ef8b75658570c115f5c1aedd69f3ef0e83f0a
dr94406 <dr94406@naver.com>
2022-11-10 23:51:24 +0900

    [feat] Interface Practice commit

---
+++ Practice/MainInter.java
@@ -0,0 +1,29 @@
+//package Practice;
+//
+//class A {
+//    void autoplay(I i){
+//        i.play();
+//    }
+//}
+//interface I {
+//    public abstract void play();
+//}
+//
+//class B implements I {
+//    public void play(){
+//        System.out.println("B ON");
+//    }
+//}
+//class C implements I {
+//    public void play(){
+//        System.out.println("C ON");
+//    }
+//}
+//
+//public class MainInter {
+//    public static void main(String args[]){
+//        A a = new A();
+//        a.autoplay(new B());
+//        a.autoplay(new C());
+//    }
+//}
