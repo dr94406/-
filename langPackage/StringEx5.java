f249423d4e390f65c9647d20df5563dca81b5d89
dr94406 <dr94406@naver.com>
2022-11-27 21:54:18 +0900

    [feat] Interface Practice commit

--- langPackage/StringEx5.java
+++ langPackage/StringEx5.java
@@ -1,13 +1,13 @@
-import java.io.UnsupportedEncodingException;
-
-public class StringEx5 {
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        String str = "가";
-
-        byte[] bArr = str.getBytes("UTF-8");
-        byte[] bArr2 = str.getBytes("CP949");
-
-        System.out.println("UTF-8:" + joinByteArr(bArr));
-        System.out.println("CP949:" + joinByteArr(bArr2));
-    }
-}
+//import java.io.UnsupportedEncodingException;
+//
+//public class StringEx5 {
+//    public static void main(String[] args) throws UnsupportedEncodingException {
+//        String str = "가";
+//
+//        byte[] bArr = str.getBytes("UTF-8");
+//        byte[] bArr2 = str.getBytes("CP949");
+//
+//        System.out.println("UTF-8:" + joinByteArr(bArr));
+//        System.out.println("CP949:" + joinByteArr(bArr2));
+//    }
+//}
